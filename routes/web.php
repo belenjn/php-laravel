899bcb4898ed43f4672ed380898c90a921fb70a5
Belen <belenjarabanavas31@gmail.com>
2022-08-08 11:17:45 +0200

    First routes done

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,42 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Rutas: Home, about, room grid, room list, room details, offers, contacts.
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
 });
 
-// Ruta de prueba: 
+Route::get('/about', function () {
+    return 'About';
+});
 
 Route::get('/contacts', function () {
     return 'Contacts';
 });
+
+Route::get('/offers', function () {
+    return 'Offers';
+});
+
+Route::get('/rooms', function () {
+    return 'Rooms';
+});
+
+Route::get('/rooms/roomlist', function () {
+    return 'Room list';
+});
+
+Route::get('/rooms/roomlist/roomdetail/{id}', function ($id) {
+    return 'Room Detail: {$id}';
+});
+
+Route::post('/rooms/{checkin}-{checkout}', function ($checkin, $checkout) {
+    if($checkin && $checkout) {
+        return "Rooms available from {$checkin} to {$checkout}";
+    } else {
+        return "there are no rooms available on that date";
+    }
+});
+
+
