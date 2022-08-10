5f33244f2aa37a678d0ccaddab1c01f16fb849d9
Belen <belenjarabanavas31@gmail.com>
2022-08-10 16:47:15 +0200

    new links on navbar to go to dif pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -34,10 +34,10 @@
                 <img src="./assets/nav/logo-nav.png" alt="logo del hotel" class="navbar__logo" />
             </div>
             <div class="navbar__categories">
-                <li>About Us</li>
-                <li>Rooms</li>
-                <li>Offers</li>
-                <li>Contact</li>
+            <a href="/about" style="text-decoration: none; color: black;"><li>About us</li>
+        <a href="/rooms" style="text-decoration: none; color: black;"><li>Rooms</li></a>
+        <a href="/offers" style="text-decoration: none; color: black;"><li>Offers</li></a>
+        <a href="/contact" style="text-decoration: none; color: black;"><li>Contact</li></a>
             </div>
             <div class="navbar__container-icons-right">
                 <img src="./assets/nav/person.png" alt="personal" class="navbar__person" />
